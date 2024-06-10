736aa4cfa01d6fcc54abdd32bee24cfca8f54ab7
Omar <Runboi992@gmail.com>
2024-06-10 16:22:10 -0600

    new

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LearnCSharp();
+        }
+
+        static void LearnCSharp()
+        {
+            Console.WriteLine("Welcome to learning C#!");
+            int number = 5;
+            string message = "The number is " + number;
+            Console.WriteLine(message);
+
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
-using System;
 
+
+using System;
+using System.Diagnostics;
 class Program
+
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
+
 }
